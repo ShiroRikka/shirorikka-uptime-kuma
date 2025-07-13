48e35fd3e5b237f56572d9d10767398fd2d49cc1
ShiroRikka <2985772684@qq.com>
2025-07-13 23:21:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "trying to restore the database (if it exists)..."
-litestream restore -if-replica-exists /app/data/kuma.db
+litestream restore -if-replica-exists -generation 4591c2cc6a46c0e1 /app/data/kuma.db
 
 echo "starting replication and the application..."
 litestream replicate
